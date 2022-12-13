e3ccb173a55096d0712d1d5fb4fa24bee274ebd4
lars110304 <lars110304@gmail.com>
2022-12-13 09:30:50 +0100

    et4g5thrynthj

--- vormule1/resources/js/components/login.vue
+++ vormule1/resources/js/components/login.vue
@@ -3,26 +3,22 @@
     <form>
         <fieldset>
             <div class="form-group">
-                <label for="exampleInputEmail1" class="form-label mt-4"
-                    >Email address</label
-                >
+                <label for="email" class="form-label mt-4">Email address</label>
                 <input
                     type="email"
                     class="form-control"
-                    id="exampleInputEmail1"
+                    id="email"
                     placeholder="Enter email"
                     required
                 />
             </div>
 
             <div class="form-group">
-                <label for="exampleInputPassword1" class="form-label mt-4"
-                    >Password</label
-                >
+                <label for="password" class="form-label mt-4">Password</label>
                 <input
                     type="password"
                     class="form-control"
-                    id="exampleInputPassword1"
+                    id="password"
                     placeholder="Password"
                     required
                 />
